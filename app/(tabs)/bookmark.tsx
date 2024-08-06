e03b62911b14182d6d929f31c8265bbcf067358c
pennimerger <emmanuelashiegbulam@gmail.com>
2024-08-06 11:42:44 +0100

    Saved

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -5,6 +5,10 @@ const Bookmark = () => {
   return (
     <View>
       <Text>Bookmark</Text>
+      {/* It is one thing to suspend a service and another 
+      to invalidate the entire projectId */}
+      {/* Never use appwrite for your backend guys */}
+      {/* I'll be back to this. */}
     </View>
   )
 }
